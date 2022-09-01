d21210bebef2db5183ee6d0fbdf5acf9c330a877
Fares Qaddoura <frqd92@gmail.com>
2022-09-01 20:40:43 +0100

    create main interface, create the logic behind the grid creation, create a settings box that will open a pop up window from above the screen

---
+++ script.js
@@ -0,0 +1,24 @@
+let drawingGrid = document.querySelector(".drawing-grid");
+
+
+
+calculateGridX(14);
+
+
+
+function calculateGridX(x){
+    let gridCalc = 600 / x ; 
+    drawingGrid.style.gridTemplateColumns =`repeat(auto-fit, ${gridCalc}px)`;
+    drawingGrid.style.gridTemplateRows =` repeat(auto-fit, ${gridCalc}px) `;
+
+
+    for ( let z=0 ; z<(x*x) ; z++ ){
+        let box = document.createElement("div");
+        box.setAttribute("class", "box");
+        drawingGrid.appendChild(box);
+    
+    }
+
+}
+
+
