a029656d7987c6550bae7dbc2a8da4b94c172eb5
Fares Qaddoura <frqd92@gmail.com>
2022-09-02 14:46:42 +0100

    risky

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ let settingsContainer = document.getElementById("settings-container");
 let settingsBtn = document.querySelector(".settings");
 let settingsCloseBtn = document.getElementById("setting-close-button")
 let currentGridSize;
+window.onload=calculateGrid(50);
+
+settingsBtn.addEventListener("click", ()=>{
+    settingsContainer.style.display="block";
+})
+window.addEventListener("click", (e)=>{
+    if(e.target === settingsContainer){
+        settingsContainer.style.display="none";
+    }
+})
+settingsCloseBtn.addEventListener("click", ()=>{
+   settingsContainer.style.display="none";
+})    
 
 
-settingsPopup();
-calculateGrid(16);
 gridSlider();
 gridOpacity();
 
@@ -24,53 +35,52 @@ function calculateGrid(x){
         drawingGrid.appendChild(box);
     }
     currentGridSize=x;
-    
-}
-
-
-
-
-function settingsPopup(){
-
-    settingsBtn.addEventListener("click", ()=>{
-        settingsContainer.style.display="block";
-    })
-    window.addEventListener("click", (e)=>{
-        if(e.target === settingsContainer){
-            settingsContainer.style.display="none";
-        }
-    })
-    settingsCloseBtn.addEventListener("click", ()=>{
-       settingsContainer.style.display="none";
-    })
+ 
 }
 
 
 function gridSlider(){
     let gridSlider = document.getElementById("gridRange");
     let gridSliderText = document.querySelector("#gridRangeValue");
+    let warningText = document.getElementById("warning-msg");
 
-    gridSlider.addEventListener("click", ()=>{
+
+    gridSlider.addEventListener("mousedown", ()=>{
         refreshGrid()
-        gridSliderText.textContent=gridSlider.value + " x " + gridSlider.value; /*  fix when sliding doesn't show right format*/
+        gridSliderText.textContent=gridSlider.value + " x " + gridSlider.value;
+        gridSlider.addEventListener("mousemove", ()=>{
+            gridSliderText.textContent=gridSlider.value + " x " + gridSlider.value; 
+
+        })
+
         calculateGrid(gridSlider.value);
-
+            if(gridSlider.value>=100){
+                warningText.classList.add("warningText");
+                warningText.innerText = "Going above 100x100 might slow down your browser!";
+ 
+            }
+            else{
+                warningText.textContent ="";
+            }
     })
-
 }
 
 function gridOpacity(){ 
     let lineSlider = document.getElementById("lineRange");
     let opacitySliderText = document.getElementById("lineRangeValue");
+
     lineSlider.addEventListener("click", ()=>{
-    let insideGrid = drawingGrid.childNodes;
-    let lineSliderVal = lineSlider.value / 100;
-    let lineSliderValPercent = Math.round(lineSliderVal * 100);
-        for(let z=0;z<(currentGridSize*currentGridSize);z++){
-            insideGrid[z].style.border= ` 0.2px solid rgba(255, 255, 255, ${lineSliderVal}) `;
-            opacitySliderText.textContent = ` ${lineSliderValPercent}% `
-        }
-    })
+
+
+
+        let insideGrid = drawingGrid.childNodes;
+        let lineSliderVal = lineSlider.value / 100;
+        let lineSliderValPercent = Math.round(lineSliderVal * 100);
+            for(let z=0;z<(currentGridSize*currentGridSize);z++){
+              insideGrid[z].style.border= ` 0.2px solid rgba(255, 255, 255, ${lineSliderVal}) `;
+               opacitySliderText.textContent = ` ${lineSliderValPercent}% `
+           }
+     })
 }
 function refreshGrid(){
     /* everytime a new grid number was chosen it added on top of the old on so had to delete contents before every change*/
